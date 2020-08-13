3826d45c32c3cf2be39328c46bb9a9a3bb9812c1
Dieferson Massarotto <dieferson@usp.br>
2020-08-13 04:17:36 -0300

    criei adm, usuario e to começando a trabalhar com o banco

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+
+    STATUS=(
+        ( 'doing','Doing'),
+        ( 'done','Done'),
+    )
+    title = models.CharField(max_length=255) ## aceita no maximo 255
+    description = models.TextField()
+    done= models.CharField(
+        max_length=5,
+        choices=STATUS,
+    )
+    created_at=models.DateTimeField(auto_now_add=True) # sempre que for criado ira criar esse valorauto_now_add=True
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title #para que la na pagina de adminstrador nao fique object 1 e 2 mas sim o titulo
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
+
 # Create your views here.
+def tasklist(request):
+    tasks= Task.objects.all()
+    return render(request,'tasks/list.html',{'tasks':tasks}) #o valor dela sao as tasks que eu resgatei do banco
 
 def helloworld(request):
     return HttpResponse('Hello World!')
 
-def tasklist(request):
-    return render(request,'tasks/list.html')
 
 def yourName(request,name):
     return render(request,'tasks/yourname.html',{'name':name})
